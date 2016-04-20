de3eff877730ffb3289b4ecb4dd31923f805c972
Justin S <JustinJSumner@gmail.com>
2016-04-20 14:56:33 -0700

    Complete level 1

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,35 @@
 require "json"
+require 'date'
 
+data = File.read('data.json')
+data_hash = JSON.parse(data)
+
+out = {"rentals": []}
+rentals = data_hash["rentals"]
+cars = data_hash["cars"]
+
+rentals.each do |rental|
+	id = rental["id"]
+	car_id = rental["car_id"]
+	distance = rental["distance"]
+	start_date =  DateTime.parse(rentals[0]["start_date"]).to_date
+	end_date =  DateTime.parse(rentals[0]["end_date"]).to_date
+
+	period = (end_date - start_date + 1).to_i
+	ppd = cars[car_id -1]["price_per_day"]
+	ppk = cars[car_id -1]["price_per_km"]
+	price = period * ppd + distance * ppk
+
+	new = {}
+	new["id"] = id 
+	new["price"] = price
+	out[:rentals].push(new)
+
+end
+
+out = JSON.pretty_generate out
+
+f = File.new("output.json", "w+")
+
+f.write(out)
 # your code
